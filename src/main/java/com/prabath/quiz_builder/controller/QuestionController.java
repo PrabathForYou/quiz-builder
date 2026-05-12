328f332717d0b9ee910d95484b53b76daee3baa5
PrabathForYou <prabathabeysighe001@gmail.com>
2026-05-12 09:15:02 +0530

    add status code for get all the question request

--- src/main/java/com/prabath/quiz_builder/controller/QuestionController.java
+++ src/main/java/com/prabath/quiz_builder/controller/QuestionController.java
@@ -1,9 +1,11 @@
 package com.prabath.quiz_builder.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
 
 import com.prabath.quiz_builder.model.Question;
 import com.prabath.quiz_builder.service.QuestionService;
@@ -15,8 +17,8 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("")
-    public List<Question> getAllQuestions() {
-        return questionService.getAllQuestions();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return new ResponseEntity<>(questionService.getAllQuestions(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
